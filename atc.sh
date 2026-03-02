708fbcabfc916697044a7cf8f7ec4145bccbc280
Matheus Ferreira de Souza <151486309+matheusf30@users.noreply.github.com>
2026-03-02 10:55:15 -0300

    Iniciando roteiro para automatização da rotina.
    Imagens do Cluster sendo copiadas para a máquina física

---
+++ atc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+######################################################################
+### ESTE SCRIPT PEGA DADOS DO IFSC NA PALMA DA MÃO                 ###
+### Exercício de Aula de Análise de Tempo e Clima 2                ### 
+###                                                                ### 
+### CTMET/IFSC                                                     ###
+###                                                                ### 
+###                                                                ### 
+### Elaborado:          MARIO QUADRO       	       -FEB, 26th 2023 ###
+### Adaptado:           MATHEUS FERREIRA DE SOUZA  -MAR, 02nd 2026 ###
+######################################################################
+
+scp -r meteoro@172.16.0.110:/media/produtos/atc/202602/2026022812 .
